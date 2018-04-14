d498e5b341832260284ab401f04deaf079af9bd9
Ahad Sheriff <ahadsheriff@gmail.com>
2018-04-14 16:35:19 -0400

    Added help status view controller

---
+++ respondFirst/HelpStatusViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HelpStatusViewController.swift
+//  respondFirst
+//
+//  Created by Ahad Sheriff on 4/14/18.
+//
+
+import UIKit
+
+class HelpStatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
