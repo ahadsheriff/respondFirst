e2f36cae9cc5fe04a257a4b5775836f105d07dc2
Joseph DeWitt <jhpdewitt@gmail.com>
2018-04-14 14:09:55 -0400

    firebase snippet added to AppDelegate

--- respondFirst/AppDelegate.swift
+++ respondFirst/AppDelegate.swift
@@ -43,3 +43,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+import UIKit
+import Firebase
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+    -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
