bdf5ae6e552fc4d06387ae6ea4e420236955acc2
Ahad Sheriff <ahadsheriff@gmail.com>
2018-04-14 15:40:49 -0400

    Added mapViewController

---
+++ respondFirst/HelpMapViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HelpMapViewController.swift
+//  respondFirst
+//
+//  Created by Ahad Sheriff on 4/14/18.
+//
+
+import UIKit
+
+class HelpMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendForHelp(_ sender: Any) {
+        print("Hello")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
