fcf4f49ca099d8cc22b0737108d30198b834fd43
Ahad Sheriff <ahadsheriff@gmail.com>
2018-04-15 04:25:13 -0400

    Switch

--- respondFirst/SaveOptionsViewController.swift
+++ respondFirst/SaveOptionsViewController.swift
@@ -9,14 +9,27 @@ import UIKit
 
 class SaveOptionsViewController: UIViewController {
     
-    var senderID: String! = ""
-
+    @IBOutlet weak var mySwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        changeText()
+        mySwitch.addTarget(self, action: #selector(switchToggled(_:)), for: UIControlEvents.valueChanged)
     }
 
+    
+    @IBAction func switchToggled(_ sender: Any) {
+        changeText()
+    }
+    
+    func changeText() {
+        if mySwitch.isOn {
+            print("Switch is on")
+        } else {
+            print("Switch is off")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
